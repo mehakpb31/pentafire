bd580d058bf6036e2e2feb7e91bd66e45528949a
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-18 19:47:56 -0800

    rename mail.js to contact.js

--- contact.js
+++ contact.js
@@ -1,26 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import {
-    getFirestore,
-    collection,
-    addDoc,
-    serverTimestamp,
-} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCkC1wtb888skiSNqeCdpzQP40wO66rTGc",
-    authDomain: "pentarivertechnologies-6fd54.firebaseapp.com",
-    databaseURL:
-        "https://pentarivertechnologies-6fd54-default-rtdb.firebaseio.com",
-    projectId: "pentarivertechnologies-6fd54",
-    storageBucket: "pentarivertechnologies-6fd54.appspot.com",
-    messagingSenderId: "55988809377",
-    appId: "1:55988809377:web:38c4f2b379504a9be10ae0",
-    measurementId: "G-WHT6CJX73Z",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 document.getElementById("contactForm").addEventListener("submit", submitForm);
 
 const contactFormDB = collection(db, "contactForm");
@@ -51,7 +28,7 @@ function submitForm(e) {
             alert(error.message);
         });
 
-    document.getElementById("contactForm").reset();   
+    document.getElementById("contactForm").reset();
 }
 
 const getElementVal = (id) => {
